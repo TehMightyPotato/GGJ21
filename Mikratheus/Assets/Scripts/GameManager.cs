7faa55849dff60239f89b654fc32d05accc58696
Nicolas Teichtweier <contact@mightypotato.de>
2021-01-29 21:11:19 +0100

    project created

---
+++ Mikratheus/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static GameManager Instance;
+    public int updateRate;
+    public EventHandler PlanetUpdate;
+    private int _currentUpdateCount;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentUpdateCount == 0)
+        {
+            PlanetUpdate?.Invoke(this,EventArgs.Empty);
+            _currentUpdateCount = updateRate;
+        }
+
+        _currentUpdateCount -= 1;
+    }
+}
---
+++ Mikratheus/Assets/Scripts/Planet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Planet : MonoBehaviour
+{
+    public int inhabitants;
+
+    public int currentFollowers;
+
+    public float eventGenerationChance;
+
+    [SerializeField] private bool eventIsActive;
+    [SerializeField] private float lastEventGeneratedTime;
+
+    private void Start()
+    {
+        GameManager.Instance.PlanetUpdate += UpdatePlanet;
+    }
+
+    private void UpdatePlanet(object sender, EventArgs e)
+    {
+        eventGenerationChance = CalcEventGenerationChance();
+        if (!eventIsActive)
+        {
+            var rng = Random.value;
+            if (eventGenerationChance >= rng)
+            {
+                GenerateEvent();
+            }
+        }
+    }
+
+    private float CalcEventGenerationChance()
+    {
+        return 0;
+    }
+
+    private void GenerateEvent()
+    {
+        lastEventGeneratedTime = Time.time;
+    }
+}
