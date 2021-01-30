3a9f594e76eebc82255b88dbb96ab887faaa87c0
Nicolas Teichtweier <contact@mightypotato.de>
2021-01-30 01:05:33 +0100

    ui mock impl. und ein bisschen blödsinn lol

---
+++ Mikratheus/Assets/Anliegen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Anliegen/Anliegen")]
+public class Anliegen : ScriptableObject
+{
+   public string Text;
+
+   public void DoStuff()
+   {
+      // do Stuff
+   }
+}
