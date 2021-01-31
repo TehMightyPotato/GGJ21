61dafc15b059265deba8a2474224e57590cc288e
Nicolas Teichtweier <contact@mightypotato.de>
2021-01-31 14:50:48 +0100

    anliegen angelegt, audio impl.

--- Mikratheus/Assets/Scripts/Anliegen.cs
+++ Mikratheus/Assets/Scripts/Anliegen.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Anliegen : ScriptableObject
 {
     public Sprite sprite;
+    public AudioClip audioClip;
     [TextArea] public string message;
     [TextArea] public string question;
     public string sender;
